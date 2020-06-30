13fa5dd69c86fd2a8cfef51e4ed89c3371bb0224
Iryna_Kazei <irinalyubushak@gmail.com>
2020-06-30 10:39:59 +0300

    Task4: completed task 4 'functions'

--- tasks/functions/functions.js
+++ tasks/functions/functions.js
@@ -3,7 +3,7 @@
  *
  */
 function add(a, b) {
-
+    return a + b;
 }
 
 /**
@@ -15,15 +15,21 @@ function add(a, b) {
  * }
  */
 function getFullName(object) {
-
+    return object.firstName + ' ' + object.lastName;
 }
+
 
 /**
  * write fuction that checks is number is odd
  * true if odd, false if even
  */
 function isOdd(n) {
-
+    let x = n % 2;
+    if (x == 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -31,7 +37,14 @@ function isOdd(n) {
  * e.g ["one", "two", "three"] should return one
  */
 function getShortest(wordArray) {
+    let shortestWord = wordArray[0];
+    for (const element of wordArray) {
+        if (element.length < shortestWord.length) {
+            shortestWord = element;
+        }
+    }
 
+    return shortestWord;
 }
 
 /**
@@ -39,7 +52,11 @@ function getShortest(wordArray) {
  * e.g getGoogle(5) should return "gooooogle"
  */
 function getGoogle(n) {
-
+    let google = "g";
+    for (let j = 1; j <= n; j++) {
+        google += "o";
+    }
+    return google + "gle";
 }
 
 /**
@@ -52,7 +69,12 @@ function getGoogle(n) {
  * }
  */
 function getUser(firstName, lastName, age) {
+    let person = {};
+    person.firstName = firstName ? firstName : null;
+    person.lastName = lastName ? lastName : null;
+    person.age = age ? age : null;
 
+    return person;
 }
 
 /**
@@ -62,18 +84,25 @@ function getUser(firstName, lastName, age) {
  */
 
 function getTotalPath(path) {
-
+    let totalPath = 0;
+    for (const element of path) {
+        totalPath += element.distance;
+    }
+    return totalPath;
 }
+
 
 /**
  * write a function that will calculate a discount considering the Amount
  * and the percentage (hint: you need to use the Closure here)
- * @param {amount} num 
- * @param {percentage} num 
+ * @param {amount} num
+ * @param {percentage} num
  */
 
 function discountFunction(amount) {
-
+    return function (percentage) {
+        return percentage - percentage * amount / 100;
+    };
 }
 
 /**
@@ -84,27 +113,29 @@ function discountFunction(amount) {
  */
 
 const myObject = {
-	name: 'John',
-	lastName: 'Doe',
-	age: 25,
-	friends: ['Mike', 'Alan', 'Daniel'],
-	keys() {
-		//write your code here
-	},
-	call() {
-		//write your code here
-	}
+    name: 'John',
+    lastName: 'Doe',
+    age: 25,
+    friends: ['Mike', 'Alan', 'Daniel'],
+    keys() {
+        for (const key in this) {
+            console.log(key);
+        }
+    },
+    call() {
+        return "My name is " + this.name + " " + this.lastName + " and I am " + this.age + " years old. My best friend is " + this.friends[2];
+    }
 
 };
 
 module.exports = {
-	add,
-	getFullName,
-	isOdd,
-	getShortest,
-	getGoogle,
-	getUser,
-	getTotalPath,
-	discountFunction,
-	myObject
+    add,
+    getFullName,
+    isOdd,
+    getShortest,
+    getGoogle,
+    getUser,
+    getTotalPath,
+    discountFunction,
+    myObject
 };
