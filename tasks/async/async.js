03c2d1a58a6bd3dde7c17db7bc1a1eec1c0aa9e8
Iryna_Kazei <irinalyubushak@gmail.com>
2020-07-28 22:20:11 +0300

    add task 8

--- tasks/async/async.js
+++ tasks/async/async.js
@@ -9,36 +9,57 @@ const {
  * console.log data got from callbackFunction
  */
 function printDataCallback() {
+    function printData(data, data1) {
+        console.log(data1);
+    }
+    callbackFunction(printData);
 }
 
 /**
  * console.log data got from promiseFunction
  */
 function printDataPromise() {
+    promiseFunction().then((data) => {
+        console.log(data);
+    });
 }
 
 /**
  * console.log data got from promiseFunction using async/await
  */
 async function printDataAsyncAwait() {
+    const data = await promiseFunction();
+    console.log(data);
 }
 
 /**
  * throw error data from callbackFunctionError
  */
 function handleErrorCallback() {
+    function printData(error) {
+        throw error;
+    }
+    callbackFunctionError(printData);
 }
 
 /**
  * throw error come from promiseFunctionError
  */
 function handlePromiseError() {
+    promiseFunctionError.catch((error) => {
+        throw error;
+    });
 }
 
 /**
  * throw error come from promiseFunctionError using async/await
  */
 async function handleAsyncAwaitError() {
+    try {
+        await promiseFunctionError();
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
